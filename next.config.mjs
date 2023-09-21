a3849426abb6a5d74f7c7fbc8546577005b7099e
SAI KUMAR <31406732+ssaikumar65@users.noreply.github.com>
2023-09-21 19:37:09 +0530

    Updates

---
+++ next.config.mjs
@@ -0,0 +1,20 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/api/test",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-store, max-age=0",
+          },
+        ],
+      },
+    ];
+  },
+};
+
+export default nextConfig;
